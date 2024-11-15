a27665c38631693b0b2e8d2ffdcd99de78477cb2
Artube1991 <artube1991@gmail.com>
2024-11-15 15:07:54 +0200

    finished with the structure

--- planets/src/App.js
+++ planets/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -16,20 +15,23 @@ function App() {
       <button className="name">NEPTUNE</button>
     </nav>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="picture-box">
+        <img src="./media/test.jpg" alt="fsdf" />
+      </div>
+      <article className="info-box">
+        <h2>PLANET NAME</h2>
+        <p className="planet-info">PLANET INFO</p>
+        <p className="link">Source: <a href="#" target="_blank">Wikipedia</a></p>
+        <div className="info-buttons">OVERVIEW</div>
+        <div className="info-buttons">OVERVIEW</div>
+        <div className="info-buttons">OVERVIEW</div>
+      </article>
+      <section>
+        <div className="fact">ROTATION TIME</div>
+        <div className="fact">REVOLUTION TIME</div>
+        <div className="fact">RADIUS</div>
+        <div className="fact">AVERAGE TEMP.</div>
+      </section>
     </div>
     </>
   );
