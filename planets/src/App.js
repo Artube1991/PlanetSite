fd961fabf023bd0b7801f8f7835e5081fc9155a7
Artube1991 <artube1991@gmail.com>
2024-11-16 00:52:02 +0200

    found the way how to render the data when user clicks buttons

--- planets/src/App.js
+++ planets/src/App.js
@@ -1,15 +1,54 @@
 import './App.css';
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import data from "./components/data";
 
 function App() {
   const [planet, setPlanet] = useState("Mercury");
+  const [link, setLink] = useState("");
+  const [overview, setOverview] = useState("");
+  const [internalStructure, setInternalStructure] = useState("");
+  const [surfaceGeology, setSurfaceGeology] = useState("");
+  const [rotationTime, setRotationTime] = useState("");
+  const [revolutionTime, setRevolutionTime] = useState("");
+  const [radius, setRadius] = useState("");
+  const [temperature, setTemperature] = useState("");
+  const [planetInfo, setPlanetInfo] = useState(overview);
+
+  const getData = (plnt) => {
+    let planetData = data.filter((element) => element.planet === plnt);
+    setPlanet(plnt);
+    setLink(planetData[0].link);
+    setOverview(planetData[0].overview);
+    setInternalStructure(planetData[0].internalStructure);
+    setSurfaceGeology(planetData[0].surfaceGeology);
+    setRotationTime(planetData[0].rotationTime);
+    setRevolutionTime(planetData[0].revolutionTime);
+    setRadius(planetData[0].radius);
+    setTemperature(planetData[0].temperature);
+    setPlanetInfo(planetData[0].overview);
+    console.log(planetData);
+    console.log(planetInfo);
+  };
+
+  useEffect(() => {
+    getData(planet);
+    console.log(overview);
+    console.log(planet);
+  }, [planet]);
+
+
+
+
+
+  // console.log(planet);
+
   return (
     <>
     <h1>THE PLANETS</h1>
     <nav>
-      <button className="name" onClick={(e) => {setPlanet("Mercury")}}>MERCURY</button>
-      <button className="name">VENUS</button>
-      <button className="name">EARTH</button>
+      <button className="name" onClick={(e) => getData("Mercury")}>MERCURY</button>
+      <button className="name" onClick={(e) => getData("Venus")}>VENUS</button>
+      <button className="name" onClick={(e) => getData("Earth")}>EARTH</button>
       <button className="name">MARS</button>
       <button className="name">JUPITER</button>
       <button className="name">SATURN</button>
@@ -21,12 +60,12 @@ function App() {
         <img src="./media/test.jpg" alt="fsdf" />
       </div>
       <article className="info-box">
-        <h2>PLANET NAME</h2>
-        <p className="planet-info">PLANET INFO</p>
+        <h2>{planet}</h2>
+        <p className="planet-info">{planetInfo}</p>
         <p className="link">Source: <a href="#" target="_blank">Wikipedia</a></p>
-        <div className="info-buttons">OVERVIEW</div>
-        <div className="info-buttons">OVERVIEW</div>
-        <div className="info-buttons">OVERVIEW</div>
+        <div className="info-buttons" onClick={(e) => setPlanetInfo(overview)}>OVERVIEW</div>
+        <div className="info-buttons" onClick={(e) => setPlanetInfo(internalStructure)}>INTERNAL STRUCTURE</div>
+        <div className="info-buttons" onClick={(e) => setPlanetInfo(surfaceGeology)}>SURFACE GEOLOGY</div>
       </article>
       <section>
         <div className="fact">ROTATION TIME</div>
